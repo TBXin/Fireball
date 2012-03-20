1b79cbdb73200687c665d1d6c92cbb331904590e
TBXin <exvoided@gmail.com>
2012-03-20 20:40:47 +0400

    Интерфейс для плагинов

---
+++ Fireball/Plugin/IPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace Fireball.Plugin
+{
+    public interface IPlugin
+    {
+        String Name { get; }
+        Int32 Version { get; }
+        Boolean HasOptions { get; }
+        void ShowOptions();
+        String Upload(Image image);
+    }
+}
