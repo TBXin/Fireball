00def3a72326b9342ca5081ace0850f5aa2df8f9
TBXin <exvoided@gmail.com>
2012-03-21 19:32:22 +0400

    Обновление плагина hostingkartinok Изменение имени плагина imgur

--- HostingKartinokPlugin/PluginBody.cs
+++ HostingKartinokPlugin/PluginBody.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.Drawing;
 using System.IO;
 using System.Net;
-using System.Drawing;
 using System.Text;
 
 namespace Fireball.Plugin
@@ -10,7 +11,7 @@ namespace Fireball.Plugin
     {
         public string Name
         {
-            get { return "HostingKartinok Plugin"; }
+            get { return "HostingKartinok"; }
         }
 
         public float Version
@@ -30,87 +31,19 @@ namespace Fireball.Plugin
 
         public string Upload(Image image)
         {
-            ImageConverter converter = new ImageConverter();
-            byte[] imageData = (byte[])converter.ConvertTo(image, typeof(byte[]));
-
-            if (imageData == null)
-                return string.Empty;
-
-            Uri uri = new Uri("http://hostingkartinok.com/process.php");
-
-            string boundary = "----------" + DateTime.Now.Ticks.ToString("x");
-            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(uri);
-            webrequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1) Gecko/20100101 Firefox/9.0.1";
-            //webrequest.Timeout = 60000;
-            webrequest.Accept = "text/html, */*";
-            webrequest.ContentType = "multipart/form-data; boundary=" + boundary;
-            webrequest.Method = "POST";
-
-            StringBuilder sb = new StringBuilder();
-            sb.Append("--" + boundary);
-            sb.Append("\r\n");
-            sb.Append("Content-Disposition: form-data; name=\"");
-            sb.Append("image_1");
-            sb.Append("\"; filename=\"");
-            sb.Append("screen.png");
-            sb.Append("\"\r\n");
-            sb.Append("Content-Type: image/png");
-            sb.Append("\r\n\r\n");
-
-            string postHeader = sb.ToString();
-            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(postHeader);
-
-            sb = new StringBuilder();
-
-            sb.Append("Content-Disposition: form-data; name=\"image_2\"; filename=\"\"\r\n\r\n\r\n");
-            sb.Append("--" + boundary);
-            sb.Append("\r\n");
-
-            sb.Append("Content-Disposition: form-data; name=\"the_tags\"\r\n\r\n\r\n");
-            sb.Append("--" + boundary);
-            sb.Append("\r\n");
-
-            sb.Append("Content-Disposition: form-data; name=\"jpeg_quality\"\r\n\r\n");
-            sb.Append("70%\r\n");
-            sb.Append("--" + boundary);
-            sb.Append("\r\n");
-
-            sb.Append("Content-Disposition: form-data; name=\"resize_to\"\r\n\r\n");
-            sb.Append("500px\r\n");
-            sb.Append("--" + boundary);
-            sb.Append("\r\n");
-
-            sb.Append("Content-Disposition: form-data; name=\"upload_type\"\r\n\r\n");
-            sb.Append("standard\r\n");
-            sb.Append("--" + boundary);
-            sb.Append("--");
-            sb.Append("\r\n");
-
-            string postFooter = sb.ToString();
-            byte[] postFooterBytes = Encoding.UTF8.GetBytes(postFooter);
-
-            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n" + boundary + "\r\n");
-
-            long length = postHeaderBytes.Length + imageData.Length + boundaryBytes.Length + postFooterBytes.Length;
-            webrequest.ContentLength = length;
-
-            Stream requestStream = webrequest.GetRequestStream();
-
-            requestStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
-            requestStream.Write(imageData, 0, imageData.Length);
-            requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
-            requestStream.Write(postFooterBytes, 0, postFooterBytes.Length);
-
-            WebResponse webResponse = webrequest.GetResponse();
-            Stream s = webResponse.GetResponseStream();
-            StreamReader sr = new StreamReader(s);
-
-            string response = sr.ReadToEnd();
+            string response = innerUploadFile("http://hostingkartinok.com/process.php", image, "image_1", "image/png", new NameValueCollection
+            {
+                { "image_2", string.Empty },
+                { "the_tags", string.Empty },
+                { "jpeg_quality", "70%" },
+                { "resize_to", "500px" },
+                { "upload_type", "standard" },
+            });
 
             const string searchPattern = "</label><input class=\"image-link\" type=\"text\" size=\"92\" onclick=\"this.select();\" value=\"";
 
             int idx = response.IndexOf(searchPattern, StringComparison.Ordinal);
-            if(idx != -1)
+            if (idx != -1)
             {
                 int idx2 = response.IndexOf("\"", idx + searchPattern.Length, StringComparison.Ordinal);
 
@@ -118,6 +51,85 @@ namespace Fireball.Plugin
                 {
                     string result = response.Substring(idx + searchPattern.Length, idx2 - (idx + searchPattern.Length));
                     return result;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private string innerUploadFile(string url, Image image, string paramName, string contentType, NameValueCollection nvc)
+        {
+            ImageConverter converter = new ImageConverter();
+            byte[] imageData = (byte[])converter.ConvertTo(image, typeof(byte[]));
+
+            if (imageData == null)
+                return string.Empty;
+
+            // Создаем "границу" и получаем её байты.
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            // Создаем веб-запрос
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.ContentType = "multipart/form-data; boundary=" + boundary;
+            webRequest.Method = "POST";
+            webRequest.KeepAlive = true;
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+
+            // Получаем поток нашего запроса, куда мы будем писать данные
+            Stream requestStream = webRequest.GetRequestStream();
+
+            // Шаблон шапки параметра формы
+            const string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+
+            // Записываем в поток все наши параметры
+            foreach (string key in nvc.Keys)
+            {
+                // Записываем "границу"
+                requestStream.Write(boundarybytes, 0, boundarybytes.Length);
+                // Формируем запись о параметра и переводим в байты
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = Encoding.UTF8.GetBytes(formitem);
+                // Записываем параметр в поток
+                requestStream.Write(formitembytes, 0, formitembytes.Length);
+            }
+
+            // Записываем границу параметра, который будет содержать данные файла
+            requestStream.Write(boundarybytes, 0, boundarybytes.Length);
+            // Шаблон шапки параметра формы с файлом
+            const string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            // Формируем шапку и переводим в байты
+            string header = string.Format(headerTemplate, paramName, "screen.png", contentType);
+            byte[] headerbytes = Encoding.UTF8.GetBytes(header);
+            // Записываем полученную шапку в поток
+            requestStream.Write(headerbytes, 0, headerbytes.Length);
+            // Записываем файл в поток
+            requestStream.Write(imageData, 0, imageData.Length);
+            // Формируем хвост и переводим в байты
+            byte[] trailer = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            // Записываем хвост в поток и закрываем его.
+            requestStream.Write(trailer, 0, trailer.Length);
+            requestStream.Close();
+
+            WebResponse webResponse = null;
+            try
+            {
+                // Получаем ответ сервера
+                webResponse = webRequest.GetResponse();
+                // Получаем поток ответа сервера
+                Stream responseStream = webResponse.GetResponseStream();
+
+                if (responseStream == null)
+                    return string.Empty;
+
+                // Вычитываем его полностью и возрващаем вычитанные данные
+                return new StreamReader(responseStream).ReadToEnd();
+            }
+            catch (Exception)
+            {
+                if (webResponse != null)
+                {
+                    webResponse.Close();
                 }
             }
 
